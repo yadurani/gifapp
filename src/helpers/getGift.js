2984981aa4f7a387b4ce05f3a2b596be716597fd
Yadurani Lopez Rivera <yadu-lopez@hotmail.com>
2021-05-06 18:58:15 -0500

    Fix: agregando proxy de la url

--- src/helpers/getGift.js
+++ src/helpers/getGift.js
@@ -1,7 +1,8 @@
 const getGif = async ( category, page ) => {
   const URL_API = 'http://api.giphy.com/v1/gifs/search?q='
   const API_KEY = '9A6jp0e4admbMZUm8bRFJ9VwZiW9r8R6'
-  const url = `${URL_API}${encodeURI(category)}&limit=10&offset=${page}&api_key=${API_KEY}`
+  const URL_PROXY = 'https://api.allorigins.win/raw?url='
+  const url = `${URL_PROXY}${URL_API}${encodeURI(category)}&limit=10&offset=${page}&api_key=${API_KEY}`
   const response = await fetch(url)
   const { data, pagination } = await response.json()
   const gifs = data.map(img => ({
