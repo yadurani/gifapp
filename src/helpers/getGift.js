57d03594e9ea7b06d1864989a9f632141495f3bc
Yadurani Lopez Rivera <yadu-lopez@hotmail.com>
2021-05-06 19:24:13 -0500

    fix: arreglando url epticion

--- src/helpers/getGift.js
+++ src/helpers/getGift.js
@@ -1,8 +1,7 @@
 const getGif = async ( category, page ) => {
-  const URL_API = 'http://api.giphy.com/v1/gifs/search?q='
+  const URL_API = 'https://api.giphy.com/v1/gifs/search?q='
   const API_KEY = '9A6jp0e4admbMZUm8bRFJ9VwZiW9r8R6'
-  const URL_PROXY = 'https://api.allorigins.win/raw?url='
-  const url = `${URL_PROXY}${URL_API}${encodeURI(category)}&limit=10&offset=${page}&api_key=${API_KEY}`
+  const url = `${URL_API}${encodeURI(category)}&limit=10&offset=${page}&api_key=${API_KEY}`
   const response = await fetch(url)
   const { data, pagination } = await response.json()
   const gifs = data.map(img => ({
